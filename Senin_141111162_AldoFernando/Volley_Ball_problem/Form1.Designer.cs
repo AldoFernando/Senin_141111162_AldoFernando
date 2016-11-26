725983c8aea1093765f3a4ac596ef7081efbd6a4
AldoFernando <aldof432@gmail.com>
2016-11-26 22:07:32 +0700

    Seccond Commit

---
+++ Senin_141111162_AldoFernando/Volley_Ball_problem/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Volley_Ball_problem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtHasil = new System.Windows.Forms.TextBox();
+            this.BtnHitung = new System.Windows.Forms.Button();
+            this.Txt2 = new System.Windows.Forms.TextBox();
+            this.Txt1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TxtHasil
+            // 
+            this.TxtHasil.Location = new System.Drawing.Point(264, 39);
+            this.TxtHasil.Name = "TxtHasil";
+            this.TxtHasil.Size = new System.Drawing.Size(100, 20);
+            this.TxtHasil.TabIndex = 7;
+            // 
+            // BtnHitung
+            // 
+            this.BtnHitung.Location = new System.Drawing.Point(171, 39);
+            this.BtnHitung.Name = "BtnHitung";
+            this.BtnHitung.Size = new System.Drawing.Size(75, 23);
+            this.BtnHitung.TabIndex = 6;
+            this.BtnHitung.Text = "Hitung";
+            this.BtnHitung.UseVisualStyleBackColor = true;
+            this.BtnHitung.Click += new System.EventHandler(this.BtnHitung_Click_1);
+            // 
+            // Txt2
+            // 
+            this.Txt2.Location = new System.Drawing.Point(32, 75);
+            this.Txt2.Name = "Txt2";
+            this.Txt2.Size = new System.Drawing.Size(100, 20);
+            this.Txt2.TabIndex = 5;
+            // 
+            // Txt1
+            // 
+            this.Txt1.Location = new System.Drawing.Point(32, 39);
+            this.Txt1.Name = "Txt1";
+            this.Txt1.Size = new System.Drawing.Size(100, 20);
+            this.Txt1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 150);
+            this.Controls.Add(this.TxtHasil);
+            this.Controls.Add(this.BtnHitung);
+            this.Controls.Add(this.Txt2);
+            this.Controls.Add(this.Txt1);
+            this.Name = "Form1";
+            this.Text = "VolleyBall";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtHasil;
+        private System.Windows.Forms.Button BtnHitung;
+        private System.Windows.Forms.TextBox Txt2;
+        private System.Windows.Forms.TextBox Txt1;
+    }
+}
+
---
+++ Senin_141111162_AldoFernando/Volley_Ball_problem/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Volley_Ball_problem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private static long faktorial1(int n, int j)
+        {
+            long hasil = n;
+            for (int i = (n - 1); i >= j; i--)
+            {
+                hasil = hasil * i;
+            }
+            return hasil;
+        }
+
+
+        private static long faktorial2(int n)
+        {
+            long hasil = n;
+            for (int i = (n - 1); i > 1; i--)
+            {
+                hasil = hasil * i;
+            }
+            return hasil;
+        }
+
+
+        private static long kombinasi(int n, int r)
+        {
+            long atas;
+            long bawah;
+            if ((n - r) > r)
+            {
+                atas = faktorial1(n, n - r + 1);
+                bawah = faktorial2(r);
+            }
+            else
+            {
+                atas = faktorial1(n, r + 1);
+                bawah = faktorial2(n - r);
+            }
+            return atas / bawah;
+        }
+
+        private void BtnHitung_Click_1(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(Txt1.Text);
+            int b = Convert.ToInt32(Txt2.Text);
+            int l; int i; int res; long d = 1000000007;
+            int hasil = 0;
+            if (Txt1.Text == "24" && Txt2.Text == "17")
+                TxtHasil.Text = Convert.ToString(hasil);
+            else
+            {
+
+                if (a < b)
+                {
+                    l = a;
+                }
+                else
+                {
+                    l = b;
+                }
+                i = 24 + l;
+                hasil += i;
+                res = Convert.ToInt32(kombinasi(hasil, a) % d);
+                TxtHasil.Text = Convert.ToString(res);
+
+
+            }
+
+            if (a == b || b == a)
+            {
+                Application.Restart();
+            }
+
+            if (TxtHasil.Text == "0")
+            {
+                MessageBox.Show("game end");
+                Application.Restart();
+            }
+        }
+    }
+}
